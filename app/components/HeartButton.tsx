755aa39aad7302f941fceb1f1c6c1ada365e5706
dtran <dtran13899@gmail.com>
2023-05-17 12:08:05 -0600

    Add HeartButton component

---
+++ app/components/HeartButton.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { User } from '@prisma/client';
+import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
+
+interface HeartButtonProps {
+  listingId: string;
+  currentUser?: User | null;
+}
+
+const HeartButton: React.FC<HeartButtonProps> = ({
+  listingId,
+  currentUser,
+}) => {
+  const hasFavorited = false;
+  const toggleFavorite = () => {};
+
+  return (
+    <div
+      className="relative transition cursor-pointer hover:opacity-80"
+      onClick={toggleFavorite}
+    >
+      <AiOutlineHeart
+        size={28}
+        className="absolute -top-[2px] -right-[2px] fill-white"
+      />
+      <AiFillHeart
+        size={24}
+        className={hasFavorited ? 'fill-rose-500' : 'fill-rose-500/60'}
+      />
+    </div>
+  );
+};
+
+export default HeartButton;
