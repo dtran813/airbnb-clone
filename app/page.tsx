6f3996cee2eb306a6fdc329452ba972322977229
dtran <dtran13899@gmail.com>
2023-05-16 18:00:58 -0600

    Add getListings action

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,20 @@
+import getListings from './actions/getListings';
 import Container from './components/Container';
 import EmptyState from './components/EmptyState';
 
-export default function Home() {
-  const isEmpty = true;
+export default async function Home() {
+  const listings = await getListings();
 
-  if (isEmpty) {
+  if (listings.length === 0) {
     return <EmptyState showReset />;
   }
 
   return (
     <Container>
       <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-        <div>My future listings</div>
+        {listings.map((listing: any) => {
+          return <div key={listing.title}>{listing.title}</div>;
+        })}
       </div>
     </Container>
   );
