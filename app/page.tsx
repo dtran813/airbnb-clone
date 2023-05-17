1e0f01212c4a6fd7b8d20ce088e6c2d27d193ff2
dtran <dtran13899@gmail.com>
2023-05-16 18:33:02 -0600

    Initialize ListingCard and add handleCancel()

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import getCurrentUser from './actions/getCurrentUser';
 import getListings from './actions/getListings';
+
 import Container from './components/Container';
 import EmptyState from './components/EmptyState';
+import ListingCard from './components/Listings/ListingCard';
 
 export default async function Home() {
   const listings = await getListings();
+  const currentUser = await getCurrentUser();
 
   if (listings.length === 0) {
     return <EmptyState showReset />;
@@ -12,9 +16,13 @@ export default async function Home() {
   return (
     <Container>
       <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-        {listings.map((listing: any) => {
-          return <div key={listing.title}>{listing.title}</div>;
-        })}
+        {listings.map((listing: any) => (
+          <ListingCard
+            key={listing.id}
+            currentUser={currentUser}
+            data={listing}
+          />
+        ))}
       </div>
     </Container>
   );
