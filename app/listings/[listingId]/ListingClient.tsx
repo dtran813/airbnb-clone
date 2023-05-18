b50043eb03a77cef25fb9fdf42e10fcd68de469e
dtran <dtran13899@gmail.com>
2023-05-18 12:29:08 -0600

    Change redirect after creating a reservation

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -73,8 +73,7 @@ const ListingClient: React.FC<ListingClientInterface> = ({
       .then(() => {
         toast.success('Listing reserved!');
         setDateRange(initialDateRange);
-        // Redirect to /trips
-        router.refresh();
+        router.push('/trips');
       })
       .catch(() => {
         toast.error('Something went wrong!');
