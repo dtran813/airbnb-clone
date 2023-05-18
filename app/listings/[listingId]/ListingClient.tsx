a1cc0b1b21d6b2f241ee1ffe200657f06d99e3d3
dtran <dtran13899@gmail.com>
2023-05-17 21:03:18 -0600

    Add route for reservations

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -2,6 +2,7 @@
 
 import axios from 'axios';
 import { toast } from 'react-hot-toast';
+import { Range } from 'react-date-range';
 import { useRouter } from 'next/navigation';
 import { useCallback, useEffect, useMemo, useState } from 'react';
 import { differenceInCalendarDays, eachDayOfInterval } from 'date-fns';
@@ -53,7 +54,7 @@ const ListingClient: React.FC<ListingClientInterface> = ({
 
   const [isLoading, setIsLoading] = useState(false);
   const [totalPrice, setTotalPrice] = useState(listing.price);
-  const [dateRange, setDateRange] = useState(initialDateRange);
+  const [dateRange, setDateRange] = useState<Range>(initialDateRange);
 
   const onCreateReservation = useCallback(() => {
     if (!currentUser) {
