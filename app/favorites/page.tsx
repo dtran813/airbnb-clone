d0bdfaaa1ca718c64c6ae4b98cd4d4e626119cf4
dtran <dtran13899@gmail.com>
2023-05-18 14:07:18 -0600

    Add favorite listings page

---
+++ app/favorites/page.tsx
@@ -0,0 +1,25 @@
+import getCurrentUser from '../actions/getCurrentUser';
+import getFavoriteListings from '../actions/getFavoriteListings';
+
+import EmptyState from '../components/EmptyState';
+import FavoriteListingClient from './FavoriteListingClient';
+
+const FavoriteListingsPage = async () => {
+  const currentUser = await getCurrentUser();
+  const listings = await getFavoriteListings();
+
+  if (listings.length === 0) {
+    return (
+      <EmptyState
+        title="No favorites found"
+        subtitle="Looks like you have no favorite listings."
+      />
+    );
+  }
+
+  return (
+    <FavoriteListingClient listings={listings} currentUser={currentUser} />
+  );
+};
+
+export default FavoriteListingsPage;
