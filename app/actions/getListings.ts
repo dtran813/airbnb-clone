7407c99c97a2fc0ab958b18f1b6d00634329b310
dtran <dtran13899@gmail.com>
2023-05-18 20:20:44 -0600

    Modify the query of getListings actiont to display the results according to the filter parameters

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -2,16 +2,77 @@ import prisma from '@/app/libs/prismadb';
 
 export interface IListingsParams {
   userId?: string;
+  category?: string;
+  locationValue?: string;
+  guestCount?: number;
+  roomCount?: number;
+  bathroomCount?: number;
+  startDate?: string;
+  endDate?: string;
 }
 
 export default async function getListings(params: IListingsParams) {
   try {
-    const { userId } = params;
+    const {
+      userId,
+      category,
+      locationValue,
+      guestCount,
+      roomCount,
+      bathroomCount,
+      startDate,
+      endDate,
+    } = params;
 
     let query: any = {};
 
     if (userId) {
       query.userId = userId;
+    }
+
+    if (category) {
+      query.category = category;
+    }
+
+    if (locationValue) {
+      query.locationValue = locationValue;
+    }
+
+    if (guestCount) {
+      query.guestCount = {
+        gte: +guestCount,
+      };
+    }
+
+    if (roomCount) {
+      query.roomCount = {
+        gte: +roomCount,
+      };
+    }
+
+    if (bathroomCount) {
+      query.bathroomCount = {
+        gte: +bathroomCount,
+      };
+    }
+
+    if (startDate && endDate) {
+      query.NOT = {
+        reservations: {
+          some: {
+            OR: [
+              {
+                startDate: { lte: startDate },
+                endDate: { gte: startDate },
+              },
+              {
+                startDate: { lte: endDate },
+                endDate: { gte: endDate },
+              },
+            ],
+          },
+        },
+      };
     }
 
     const listings = await prisma.listing.findMany({
