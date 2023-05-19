87f07e38cacfb71eb0e5d30d1e03808bc9c9a6a8
dtran <dtran13899@gmail.com>
2023-05-19 15:05:44 -0600

    Remove unused file and add input focus when click on label

--- app/components/Inputs/Input.tsx
+++ app/components/Inputs/Input.tsx
@@ -47,10 +47,12 @@ const Input: React.FC<InputProps> = ({
         }`}
       />
       <label
+        htmlFor={id}
         className={`absolute top-5 z-10 text-md duration-150 transform -translate-y-3 origin-[0] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4
         ${formatPrice ? 'left-9' : 'left-4'}
         ${errors[id] ? 'text-rose-500' : 'text-zinc-400'}
-        `}>
+        `}
+      >
         {label}
       </label>
     </div>
--- app/hooks/useKeydown.ts
+++
@@ -1,5 +0,0 @@
-import { useEffect } from 'react';
-
-const useEscapeKey = (callback: Function) => {};
-
-export default useEscapeKey;
